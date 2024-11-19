3fbc767b34a15fdf6ef7aa9d89ad207585dcfc6f
Alexey Ermolenko <alexey.ermolenko@saritasa.com>
2024-11-19 09:41:52 +0500

    add first commands

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	scanner := bufio.NewScanner(os.Stdin)
+	cliCommands := getCliCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			break
+		}
+
+		command := scanner.Text()
+		cli, ok := cliCommands[command]
+		if !ok {
+			continue
+		}
+
+		err := cli.callback()
+
+		if err != nil {
+			break
+		}
+	}
 }
